137ed7442b15faba719550f24a0f697dcc9b1afd
Aleksandr Karpov <keyfour13@gmail.com>
2015-11-26 12:09:35 +0300

    Contitnue writing script

--- kernlogparser.sh
+++ kernlogparser.sh
@@ -11,18 +11,30 @@ OUTDIR="/tmp/kernlogparser.output"
 #Default file for parsing
 PFILE="./logfile.txt"
 
+#Path to temp logfile
+LOGFILE=$OUTDIR/kernlogparser.logfile
+
+#Timestamp pattern
+TIMESTAMP="[*.*]"
+
 #Check if $OUTDIR presents
 [[ ! -d $OUTDIR ]] && mkdir $OUTDIR
 [[ ! -d $OUTDIR ]] && { echo "Can't create $OUTDIR"; exit 2; }
 
 # check argument
+# read log file from argument
 [[ ! "$1" == "" ]] && PFILE=$1
 [[ ! -f $PFILE ]] && { echo "There is no such file $FILE"; exit 2; }
 
-# read log file from argument
+cp -f $PFILE $LOGFILE
+
 # replace add function names to address with pykernmap
 # save to temporary file
 # remove strings without timestamps
+kernmap.py -m $KERNDIR/System.map -t $LOGFILE -r > $OUTDIR/tmp.txt
+[[ -f $OUTDIR/tmp ]] && mv -f $OUTDIR/tmp $LOGFILE
+grep $TIMESTAMP $LOGFILE > $OUTDIR/tmp
+[[ -f $OUTDIR/tmp ]] && mv -f $OUTDIR/tmp $LOGFILE
 
 # open updated file, read line by line, filter (remove) timestamp
 # search in kernel directory string messages from log, use symbols : [ ] ( )
